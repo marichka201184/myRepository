cb5e6d23c0bdf36695b338b9b2aee3e1d44106a4
mbabiychuk <marichka201184@gmail.com>
2020-12-14 09:45:47 -0500

    new commit

--- hm6.py
+++ hm6.py
@@ -4,3 +4,49 @@
 # -создать метод который принимает текст и выводит список файлов (записать текст в выбранный файл)
 # -создать метод который выводит список файлов, и при выборе, выводит содержимое в консоль
 # -создать метод который даёт возможность выбрать два файла и меняет их содержимое местами
+class File:
+    def __init__(self, x, y):
+        self.list = [x, y]
+
+    def get_file(self):
+        return self.list
+
+    def __add__(self, other):
+        return f'{self.__dict__} : {other.__dict__}'
+
+    def add(self, file_name):
+        self.list.append(file_name)
+        return self.list
+
+
+file_list = File('text.txt', 'text2.txt')
+print(file_list)
+print(file_list.get_file())
+file_list.add('text3.txt')
+print(file_list.get_file())
+file_list.add('text1.txt')
+print(file_list.get_file())
+
+
+def check(files):
+    for i in files:
+        open(i, "w+")
+
+
+check(file_list.get_file())
+
+
+def wright(text, l):
+    for i in l:
+        print(i)
+    choice = input('select a file: ')
+    print(choice)
+    f1 = open(choice, 'w')
+    f1.write(text)
+    f1.close()
+    f1 = open(choice, 'r')
+    f = f1.read()
+    print(f)
+
+
+wright('ABCDEFG', file_list.get_file())
--- test.py
+++ test.py
@@ -1,35 +1,6 @@
-class Letter:
-    # 3) створити змінну класу __count.
-    __count = 0
+import os
 
-    # 4) при створенні об'єкта має створюватись змінна об'єкта(пропертя) __text, для цієї змінної мають бути гетер і сетер
-    def __init__(self, text):
-        self.__text = text
-
-    def __str__(self):
-        return f'{self.__dict__}'
-
-
-def get_text(self):
-    return self.__text
-
-
-def set_text(self, text):
-    self.__text = text
-
-
-def cnt(self):
-    self.__count += 1
-    return self.__count
-
-
-# 5) при створені об'єкта __count має збільшуватися на 1
-# 6) має бути метод(метод класу) для виводу __сount
-
-
-# 7) має бути метод який записує в наш ліст текст з нашої змінної __text
-let1 = Letter('abcd')
-let1.cnt()
-print(let1.count)
-let2 = Letter('abcd')
-print(let2)
+file_path = os.path.join('files', 'text.txt')
+#print(join)
+file = open('text.txt')
+print(type(file))
